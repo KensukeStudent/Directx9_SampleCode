bdabc1659fe200728235d768468640dae4a60e04
nagase <nagaken8937@gmail.com>
2025-04-15 23:16:15 +0900

    3_6_3D

---
+++ 3_6_Xfile/3_6_Xfile/main.h
@@ -0,0 +1,40 @@
+//-------------------------------------------------------------
+// File: main.h
+//
+// Desc: Xファイル
+//-------------------------------------------------------------
+#pragma once
+
+
+
+//-------------------------------------------------------------
+// Name: class CMyD3DApplication
+// Desc: アプリケーションのクラス
+//-------------------------------------------------------------
+class CMyD3DApplication : public CD3DApplication
+{
+    CD3DMesh* m_pMesh;		// ★★★追加
+
+
+    BOOL					m_bLoadingApp;	// ロード中？
+    CD3DFont* m_pFont;		// フォント
+
+protected:
+    virtual HRESULT OneTimeSceneInit();
+    virtual HRESULT InitDeviceObjects();
+    virtual HRESULT RestoreDeviceObjects();
+    virtual HRESULT InvalidateDeviceObjects();
+    virtual HRESULT DeleteDeviceObjects();
+    virtual HRESULT Render();
+    virtual HRESULT FrameMove();
+    virtual HRESULT FinalCleanup();
+    virtual HRESULT ConfirmDevice(D3DCAPS9*, DWORD, D3DFORMAT);
+
+    HRESULT RenderText();
+
+public:
+    LRESULT MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    CMyD3DApplication();
+    virtual ~CMyD3DApplication();
+};
+
