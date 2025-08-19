2b7d32c4887aeb70cbde70324048e21ec1519bba
nagase <nagaken8937@gmail.com>
2025-08-19 21:51:23 +0900

    7_6_1_Mono

---
+++ 7_6_1_Mono/main/hlsl.fx
@@ -0,0 +1,73 @@
+// ------------------------------------------------------------
+// セピアフィルタ
+// 
+// Copyright (c) 2003 IMAGIRE Takashi. All rights reserved.
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// テクスチャ
+// ------------------------------------------------------------
+texture SrcMap;
+sampler SrcSamp = sampler_state
+{
+    Texture = <SrcMap>;
+    MinFilter = LINEAR;
+    MagFilter = LINEAR;
+    MipFilter = NONE;
+
+    AddressU = Clamp;
+    AddressV = Clamp;
+};
+// ------------------------------------------------------------
+// 頂点シェーダからピクセルシェーダに渡すデータ
+// ------------------------------------------------------------
+struct VS_OUTPUT
+{
+    float4 Pos            : POSITION;
+    float2 Tex            : TEXCOORD0;
+};
+
+// ------------------------------------------------------------
+// 頂点シェーダプログラム
+// ------------------------------------------------------------
+VS_OUTPUT VS (
+      float4 Pos    : POSITION,          // モデルの頂点
+      float4 Tex    : TEXCOORD0             // テクスチャ座標
+){
+    VS_OUTPUT Out = (VS_OUTPUT)0;        // 出力データ
+    
+    // 位置座標
+    Out.Pos = Pos;
+    // テクスチャ座標
+    Out.Tex = Tex;
+    
+    return Out;
+}
+
+// ------------------------------------------------------------
+// ピクセルシェーダプログラム
+// ------------------------------------------------------------
+float4 PS(VS_OUTPUT In) : COLOR
+{   
+    float4 Out = (float4)0;
+    float3 YCbCr;
+    
+    const float3 RGB2Y = {0.29900, 0.58700, 0.11400};
+
+    float3 Color = tex2D( SrcSamp, In.Tex ).xyz; // 元になる色
+    
+    Out.rgb = dot(Color, RGB2Y);    // Yをそのまま色へ
+    
+    return Out;
+}
+// ------------------------------------------------------------
+// テクニック
+// ------------------------------------------------------------
+technique TShader
+{
+    pass P0
+    {
+        VertexShader = compile vs_1_1 VS();
+        PixelShader  = compile ps_2_0 PS();
+    }
+}
---
+++ 7_6_1_Mono/main/main.h
@@ -0,0 +1,83 @@
+//-------------------------------------------------------------
+// File: main.h
+//
+// Desc: セピアフィルタ
+//-------------------------------------------------------------
+#pragma once
+
+
+
+//-------------------------------------------------------------
+// 定義や定数
+//-------------------------------------------------------------
+// 現在の入力データを保存する構造体
+struct UserInput
+{
+    BOOL bRotateUp;
+    BOOL bRotateDown;
+    BOOL bRotateLeft;
+    BOOL bRotateRight;
+    BOOL bZoomIn;
+    BOOL bZoomOut;
+    BOOL bDispersionUp;
+    BOOL bDispersionDown;
+};
+
+
+
+
+//-------------------------------------------------------------
+// Name: class CMyD3DApplication
+// Desc: アプリケーションのクラス
+//-------------------------------------------------------------
+class CMyD3DApplication : public CD3DApplication
+{
+    CD3DMesh* m_pMesh;
+    CD3DMesh* m_pMeshBg;
+
+    // シェーダ
+    LPD3DXEFFECT		    m_pEffect;		// エフェクト
+    D3DXHANDLE				m_hTechnique;	// テクニック
+    D3DXHANDLE				m_hafWeight;	// 重みの配列
+    D3DXHANDLE				m_htSrcMap;		// テクスチャ
+
+    // シャドウマップ
+    LPDIRECT3DSURFACE9		m_pMapZ;			// 深度バッファ
+    LPDIRECT3DTEXTURE9		m_pOriginalMap;		// テクスチャ
+    LPDIRECT3DSURFACE9		m_pOriginalMapSurf;	// サーフェス
+    LPDIRECT3DTEXTURE9		m_pPostMap;		// テクスチャ
+    LPDIRECT3DSURFACE9		m_pPostMapSurf;	// サーフェス
+
+    // 通常の座標変換行列
+    D3DXMATRIX				m_mWorld;
+    D3DXMATRIX				m_mView;
+    D3DXMATRIX				m_mProj;
+
+    BOOL					m_bLoadingApp;	// ロード中？
+    CD3DFont* m_pFont;		// フォント
+    UserInput				m_UserInput;	// 入力データ
+
+    FLOAT                   m_fWorldRotX;   // Ｘ軸回転
+    FLOAT                   m_fWorldRotY;   // Ｙ軸回転
+    FLOAT                   m_fViewZoom;    // 視点の距離
+
+protected:
+    virtual HRESULT OneTimeSceneInit();
+    virtual HRESULT InitDeviceObjects();
+    virtual HRESULT RestoreDeviceObjects();
+    virtual HRESULT InvalidateDeviceObjects();
+    virtual HRESULT DeleteDeviceObjects();
+    virtual HRESULT Render();
+    virtual HRESULT FrameMove();
+    virtual HRESULT FinalCleanup();
+    virtual HRESULT ConfirmDevice(D3DCAPS9*, DWORD, D3DFORMAT);
+
+    HRESULT RenderText();
+
+    void    UpdateInput(UserInput* pUserInput);
+public:
+    LRESULT MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    CMyD3DApplication();
+    virtual ~CMyD3DApplication();
+};
+
